79cd594361caec376796b744a7c3165f3e48155a
Nawa Manusitthipol <nawa@nawaman.net>
2018-02-10 23:33:37 -0600

    Add Nullable

---
+++ src/main/java/nawaman/nullable/Nullable.java
@@ -0,0 +1,181 @@
+package nawaman.nullable;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import lombok.val;
+
+/**
+ * An implementation of Optional using interface and default methods.
+ * 
+ * This interface fully implements Java Optional exception for a few behaviours
+ * Being an interface allowing it to be extended -- look at an example in the unit test.
+ * 
+ * Differences from Optional.
+ * <ul>
+ *   <li>Method `of` accepts both `null` and non `null` value.</li>
+ *   <li>Method `get` does not throw NullPointerException if the value is null.</li>
+ *   <li>Methods from Object class are not override (hashCode, equals, toString).</li>
+ * </ul>
+ * 
+ * @param <TYPE>  the data type of the value wrapped by this Nullable.
+ * 
+ * @author NawaMan -- nawa@nawaman.net
+ */
+@FunctionalInterface
+public interface Nullable<TYPE> {
+    
+    /**
+     * Returns the Nullable value of the given value.
+     * 
+     * @param theGivenValue  the given value.
+     * @return  the Nullable of the given value.
+     */
+    public static <OBJECT> Nullable<OBJECT> of(OBJECT theGivenValue) {
+        return (Nullable<OBJECT>)()->theGivenValue;
+    }
+    
+    /**
+     * Return Nullable which has no value.
+     * 
+     * @return  the Nullable of the no value.
+     */
+    public static <OBJECT> Nullable<OBJECT> empty() {
+        return (Nullable<OBJECT>)()->null;
+    }
+    
+    
+    /**
+     * Returns the value.
+     * 
+     * @return  the value of hold by this nullable.
+     */
+    public TYPE get();
+    
+    
+    /**
+     * Returns the value if it is not null or the fallbackValue otherwise
+     * 
+     * @param fallbackValue  the fallback value.
+     * @return  the value or the fallback value.
+     */
+    public default TYPE orElse(TYPE fallbackValue) {
+        val value = get();
+        return (value != null) ? value : fallbackValue;
+    }
+    
+    /**
+     * Returns the value if it is not null or the value from the fallbackSupplier otherwise
+     * 
+     * @param fallbackSupplier  the fallbackSupplier.
+     * @return  the value or the fallback value.
+     */
+    public default TYPE orElseGet(Supplier<? extends TYPE> fallbackSupplier) {
+        val value = get();
+        return (value != null) ? value : fallbackSupplier.get();
+    }
+    
+    /**
+     * Returns the value if it is not null or throw the exception from the exceptionSupplier
+     * 
+     * @param exceptionSupplier  the exception supplier.
+     * @return  the value.
+     * @throws THROWABLE  if the value is null.
+     */
+    public default <THROWABLE extends Throwable> TYPE orElseThrow(Supplier<? extends THROWABLE> exceptionSupplier) throws THROWABLE {
+        val value = get();
+        if (value == null)
+            throw exceptionSupplier.get();
+        
+        return get();
+    }
+    
+    /**
+     * Returns this Nullable object if the value is null or fail the condition test otherwise return empty Nullable.
+     * 
+     * @param theCondition 
+     * @return  this object or empty Nullable.
+     */
+    public default Nullable<TYPE> filter(Predicate<? super TYPE> theCondition) {
+        val value = get();
+        if (value == null)
+            return this;
+        
+        val isPass = theCondition.test(value);
+        if (!isPass)
+            return Nullable.empty();
+        
+        return this;
+    }
+    
+    /**
+     * Apply the mapper if the value is not null then return the result Nullable otherwise return empty Nullable.
+     * 
+     * @param mapper  the mapper.
+     * @return  the result Nullable or empty Nullable.
+     */
+    public default <TARGET> Nullable<TARGET> flatMap(Function<? super TYPE, Nullable<TARGET>> mapper) {
+        val value = get();
+        if (value == null)
+            return Nullable.empty();
+            
+        val newNullableValue = mapper.apply(value);
+        return newNullableValue;
+    }
+    
+    /**
+     * Apply the mapper if the value is not null then return the result wrapped with Nullable otherwise return empty Nullable.
+     * 
+     * @param mapper  the mapper.
+     * @return  the result Nullable or empty Nullable.
+     */
+    public default <TARGET> Nullable<TARGET> map(Function<? super TYPE,? extends TARGET> mapper) {
+        val value = get();
+        if (value == null)
+            return Nullable.empty();
+            
+        val newValue = mapper.apply(value);
+        return Nullable.of(newValue);
+    }
+    
+    /**
+     * Check if the value is not null.
+     * 
+     * @return {@code true} if the value is not null.
+     */
+    public default boolean isPresent() {
+        val value = get();
+        return (value != null);
+    }
+    
+    /**
+     * If the value is not null, pass it to the consumer.
+     * 
+     * @param theConsumer  the consumer.
+     * @return  the value.
+     */
+    public default TYPE ifPresent(Consumer<? super TYPE> theConsumer) {
+        val value = get();
+        if (value != null)
+            theConsumer.accept(value);
+        
+        return value;
+    }
+    
+    /**
+     * If the value is not null, run the action.
+     * 
+     * @param theAction  the action.
+     * @return  the value.
+     */
+    public default TYPE ifPresent(Runnable theAction) {
+        val value = get();
+        if (value != null)
+            theAction.run();
+        
+        return value;
+    }
+    
+}
---
+++ src/test/java/nawaman/nullable/NullableTest.java
@@ -0,0 +1,64 @@
+package nawaman.nullable;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import lombok.val;
+
+@SuppressWarnings("javadoc")
+public class NullableTest {
+    
+    public static interface Person {
+        
+        public String getName();
+        public void setName(String name);
+        
+    }
+    
+    public static class PersonImpl implements Person {
+        private String name;
+        public PersonImpl(String name) {
+            this.name = name;
+        }
+        public String getName() { return name; }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    
+    @FunctionalInterface
+    public static interface NullablePerson extends Person, Nullable<Person>{
+        
+        public static NullablePerson of(Person person) {
+            return ()->person;
+        }
+        
+        public default String getName() {
+            return this.map(Person::getName).orElse(null);
+        }
+        
+        public default void setName(String name) {
+            this.ifPresent(()->{
+                this.get().setName(name);
+            });
+        }
+        
+    }
+    
+    @Test
+    public void test() {
+        val person1 = NullablePerson.of(new PersonImpl("Jack"));
+        val person2 = NullablePerson.of(null);
+        
+        assertEquals("Jack", person1.getName());
+        assertEquals(null,   person2.getName());
+        
+        person1.setName("John");
+        person2.setName("John");
+        
+        assertEquals("John", person1.getName());
+        assertEquals(null,   person2.getName());
+    }
+    
+}
