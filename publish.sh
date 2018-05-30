89f56e494bc28c10553ff2ca1926677534ded9c1
NawaMan <nawa@nawaman.net>
2018-05-30 17:02:45 -0600

    Update due to gradle problem.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ PUBLISHREPO=../nawaman-maven-repository
 if [ -d $PUBLISHREPO ]; then
     cp -Rf $PROJECTREPO $PUBLISHREPO
 else
-    (>&2 echo "Publish repositoy does not exist!")
+    (>&2 echo "Publish repositoy does not exist.")
 fi
